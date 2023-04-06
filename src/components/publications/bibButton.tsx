b17f8e3e27d36dd5e09b1342ab1a332bf169b93e
Harold Ruiter <haroldruiter28@gmail.com>
2023-04-06 16:27:43 +0200

    Add BibTeX export button to publications page

---
+++ src/components/publications/bibButton.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+export default function BibButton(props) {
+  const [cooldown, setCooldown] = useState(false);
+
+  return (
+    <div className="flex w-auto">
+      <button
+        id="command"
+        onClick={() => {
+          if (cooldown === false) {
+            navigator.clipboard.writeText(props.bibtex);
+            setCooldown(true);
+
+            setTimeout(() => {
+              setCooldown(false);
+            }, 1000);
+          }
+        }}
+        className="flex w-auto flex-row gap-2 rounded-lg bg-dnt-orange-200 py-1 px-2 hover:cursor-pointer hover:bg-dnt-orange-200/50 dark:bg-dnt-orange-800/50 hover:dark:bg-dnt-orange-700/50 "
+      >
+        {props.text}
+        <svg
+          id={`copy-icon ${props.bibtex}`}
+          className={cooldown ? "hidden" : ""}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+          <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+        </svg>
+        <svg
+          id={`check-icon ${props.bibtex}`}
+          className={cooldown ? "" : "hidden"}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <polyline
+            points="20 6 9 17 4 12"
+            className="animate-checkmark"
+            strokeDasharray={450}
+            strokeDashoffset={30}
+          />
+        </svg>
+      </button>
+    </div>
+  );
+}
--- src/components/publications/publication.ts
+++ src/components/publications/publication.ts
@@ -4,6 +4,13 @@ export interface IPublication {
   title: string;
   authors: string[];
   abstract?: string;
+  type?: string;
+  journal?: string;
+  volume?: string;
+  pages?: string;
+  number?: string;
+  year: string;
+  date: string;
   tags: string[];
   file: string;
 }
@@ -13,8 +20,40 @@ export const Publication = z.object({
   authors: z.string().array(),
   abstract: z.string().optional(),
   tags: z.string().array(),
+  type: z.string().optional(),
+  journal: z.string().optional(),
+  volume: z.string().optional(),
+  number: z.string().optional(),
   date: z.string(),
   year: z.string(),
   id: z.string(),
   file: z.string(),
-}); 
+});
+
+export const to_bibtex = (pub: IPublication): string => {
+  const type = pub.type ?? "misc";
+
+  let bibtex = `@${type} {
+  title = ${pub.title},
+  authors = ${pub.authors},
+  year = ${pub.year},
+  date = ${pub.date}`;
+
+  if (pub.abstract) {
+    bibtex = `${bibtex},\n  abstract = ${pub.abstract}`
+  }
+
+  if (pub.journal) {
+    bibtex = `${bibtex},\n  journal = ${pub.journal}`
+  }
+
+  if (pub.volume) {
+    bibtex = `${bibtex},\n  volume = ${pub.volume}`
+  }
+
+  if (pub.number) {
+    bibtex = `${bibtex},\n  number = ${pub.number}`
+  }
+
+  return `${bibtex}\n}`;
+}
