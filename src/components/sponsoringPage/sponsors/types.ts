67dd3973c7822a2fad48b535aaa4c34e5c35c159
Madelon Bernardy <madelon.bernardy@student.uva.nl>
2023-10-10 10:31:58 +0200

    wip

--- src/components/sponsoringPage/sponsors/types.ts
+++ src/components/sponsoringPage/sponsors/types.ts
@@ -13,20 +13,6 @@ export const SponsorListLevel1: Sponsor[] = [{
     logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
     level: 1
   },
-  {
-    name: "Zeta Alpha",
-    description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
-    url: "https://www.zeta-alpha.com/",
-    logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
-    level: 1
-  },
-  {
-    name: "Zeta Alpha",
-    description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
-    url: "https://www.zeta-alpha.com/",
-    logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
-    level: 1
-  },
   ];
 
   export const SponsorListLevel2: Sponsor[] = [{
@@ -43,11 +29,27 @@ export const SponsorListLevel1: Sponsor[] = [{
     logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
     level: 2
   },
+  ];
+
+  export const SponsorListLevel3: Sponsor[] = [{
+    name: "Zeta Alpha",
+    description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    url: "https://www.zeta-alpha.com/",
+    logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    level: 3
+  },
   {
     name: "Zeta Alpha",
     description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
     url: "https://www.zeta-alpha.com/",
     logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
-    level: 2
+    level: 3
+  },
+  {
+    name: "Zeta Alpha",
+    description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    url: "https://www.zeta-alpha.com/",
+    logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    level: 3
   },
   ];
