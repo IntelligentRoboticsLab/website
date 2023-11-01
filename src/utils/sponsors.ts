31054a1437bf51b0dae31b1f8f41cd597e17dbc2
Madelon Bernardy <madelon.bernardy@student.uva.nl>
2023-11-01 09:55:28 +0100

    Transports sponsors to utils

---
+++ src/utils/sponsors.ts
@@ -0,0 +1,77 @@
+import type { Sponsor } from "../components/sponsoringPage/sponsors/types";
+
+  // UNCOMMENT TO TEST THE DIFFERENT LEVELS
+  export const SponsorListLevel1: Sponsor[] = [
+    // {
+    //   name: "Zeta Alpha",
+    //   description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    //   url: "https://www.zeta-alpha.com/",
+    //   logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    //   level: 1
+    // },
+    ];
+  
+    export const SponsorListLevel2: Sponsor[] = [
+    //   {
+    //   name: "Zeta Alpha",
+    //   description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    //   url: "https://www.zeta-alpha.com/",
+    //   logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    //   level: 2
+    // },
+    // {
+    //   name: "Zeta Alpha",
+    //   description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    //   url: "https://www.zeta-alpha.com/",
+    //   logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    //   level: 2
+    // },
+    ];
+  
+    export const SponsorListLevel3: Sponsor[] = [
+    //   {
+    //   name: "Zeta Alpha",
+    //   description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    //   url: "https://www.zeta-alpha.com/",
+    //   logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    //   level: 3
+    // },
+    // {
+    //   name: "Zeta Alpha",
+    //   description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    //   url: "https://www.zeta-alpha.com/",
+    //   logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    //   level: 3
+    // },
+    {
+      name: "Zeta Alpha",
+      description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+      url: "https://www.zeta-alpha.com/",
+      logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+      level: 3
+    },
+    ];
+  
+    export const SponsorListLevel4: Sponsor[] = [
+    //   {
+    //   name: "Zeta Alpha",
+    //   description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    //   url: "https://www.zeta-alpha.com/",
+    //   logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    //   level: 4
+    // },
+    // {
+    //   name: "Zeta Alpha",
+    //   description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    //   url: "https://www.zeta-alpha.com/",
+    //   logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    //   level: 4
+    // },
+    // {
+    //   name: "Zeta Alpha",
+    //   description: "Zeta Alpha is the best Neural Discovery Platform for AI and beyond. Use state-of-the-art Neural Search and Generative AI to improve how you and your team discover, organize and share internal knowledge. Make better decisions, avoid reinventing the wheel, make staying in the know effortless, and make an impact with your work faster.",
+    //   url: "https://www.zeta-alpha.com/",
+    //   logo: "https://static.wixstatic.com/media/4f8123_1d56194d420648ec907078f5fc83ade1~mv2.png/v1/crop/x_0,y_54,w_4420,h_772/fill/w_336,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo%20blue.png",
+    //   level: 4
+    // },
+    ];
